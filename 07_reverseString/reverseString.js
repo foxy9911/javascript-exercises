ae8df34c9713d4038ee984b6d4ac23258daf45fa
Fox <alexandru.pricop@bestvalue.eu>
2025-09-10 22:04:36 +0300

    reverseString done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let arrayText = text.split("");
+    let reversedArray = arrayText.reverse();
+    return reversedArray.join('');
 };
 
+console.log(reverseString('hello world'));
 // Do not edit below this line
 module.exports = reverseString;
