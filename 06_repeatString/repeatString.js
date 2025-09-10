2095672ff2eb17f8390e7bbd1e631d18ef7ab02f
Fox <alexandru.pricop@bestvalue.eu>
2025-09-10 21:37:39 +0300

    repeatString Completed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let newString = '';
+    for (let i = 1; i <= num; i++){
+       newString = newString + string;
+    }
+    return newString;
 };
+
+console.log(repeatString('pula,', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
