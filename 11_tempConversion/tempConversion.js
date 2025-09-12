67729864ef501d4d90df64de90ea96603c9df4c7
Fox <alexandru.pricop@bestvalue.eu>
2025-09-12 14:15:02 +0300

    tempConversion Done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    // (x - 32) * 5/9
+
+    const conversion = (fahrenheit - 32) * 5/9;
+    return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    // (X * 9/5 + 32) F
+
+    const conversion = celsius * 9/5 + 32;
+    return Math.round(conversion * 10) / 10;
+
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
