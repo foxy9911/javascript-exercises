0054cacbd18977710c40f487674d55f9382d02b5
Fox <alexandru.pricop@bestvalue.eu>
2025-09-12 12:38:12 +0300

    removeFromArray Done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...itemToRemove) {
+    // let indexToRemove = array.indexOf(itemToRemove);
+    // array.splice(indexToRemove, 1);
+    // return array;
 
+    const newArray = [];
+    array.forEach(function (item) {
+        if (!itemToRemove.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
+}
+
+console.log(removeFromArray([2, 2, 2, 4],  2));
 // Do not edit below this line
 module.exports = removeFromArray;
