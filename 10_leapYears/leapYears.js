d2769f2b6b561a8f316a4fbacf4549135578d010
Fox <alexandru.pricop@bestvalue.eu>
2025-09-12 13:46:56 +0300

    leapYears Done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+/* Se divid cu 4
+ Se divid cu 100 doar daca se divid si cu 400 */
+const leapYears = function(year) {
 
+    if (year % 4 === 0){
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
+//Varianta mai scurta
+//return year % 100 === 0 && year % 400 === 0;
+
+
+console.log(leapYears(2000))
 // Do not edit below this line
 module.exports = leapYears;
