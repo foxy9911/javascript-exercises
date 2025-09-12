3115ee5d70b0b441be0b895a535ebf24087916c5
Fox <alexandru.pricop@bestvalue.eu>
2025-09-12 13:00:19 +0300

    sumAll Done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    const newArray=[];
 
+     if (start < 0 || end < 0 || typeof start != typeof end){
+        return "ERROR";
+    }
+    else if (start >= end) {
+        while (start >= end){
+            newArray.push(start);
+            start--;
+        }
+        console.log(newArray);
+
+    } else if (start <= end){
+        while (start <= end) {
+            newArray.push(start);
+            start++;
+        }
+    }
+
+    return newArray.reduce((sum, current) => sum + current);
 };
 
+console.log(sumAll(10,'4'));
 // Do not edit below this line
 module.exports = sumAll;
